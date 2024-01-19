7e6ec9e0685342b51ac47160bf26721d5f9e298b
Leewon2 <thehee0407@naver.com>
2024-01-20 00:46:22 +0900

    FE 로그인 화면 CSS 적용

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./Home.css"
+import "./fonts/fonts.css"
 const Home = () =>{
     
     const handleKakaoLoginClick = () =>{
         window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
     }
     return(
-        <div>
+        <div className="home-container">
+            <div className="content-box">
+        <div className="font-style">오늘 뭐 먹지?</div>
     
-            <button onClick={handleKakaoLoginClick}>
-                카카오로그인
-            </button>
+        <button onClick={handleKakaoLoginClick} className="kakao-login-btn">카카오로그인</button>
+    </div>
     
         </div>
     
